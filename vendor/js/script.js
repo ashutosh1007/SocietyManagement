6652918aafaa388476af7fae7ff66d1ee6ee5c6a
ashutosh1007 <ashutoshtambe1007@gmail.com>
2020-12-06 01:39:40 +0530

    index page completed

---
+++ vendor/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {    
+//    $('body').on('click', 'li', function() {
+//        $('li.active').removeClass('active');
+//        $(this).addClass('active');
+//    });
+});
+
+
